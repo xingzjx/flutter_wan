7c912a489fc1411c6470e08484361d8afe461b75
xingzjx <1181313247@qq.com>
2019-10-18 21:53:46 +0800

    feat:改名

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/xingzjx/files/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/xingzjx/project/flutter/flutter_abc"
-export "FLUTTER_TARGET=/Users/xingzjx/project/flutter/flutter_abc/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/xingzjx/project/flutter/flutter_wan"
+export "FLUTTER_TARGET=/Users/xingzjx/project/flutter/flutter_wan/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/xingzjx/files/flutter/bin/cache/artifacts/engine/ios"
